2bde61e72534df133695bf420d8d1a46ce120fb6
edmilson <edmilson.nlf@gmail.com>
2019-06-01 20:46:24 -0300

    aula 05 importação

---
+++ aula05/aula05-exemploimport.py
@@ -0,0 +1,7 @@
+from aula05_metodo_crawller import *
+
+print("Dollar: " + str(getDollar()))
+print("Euro: " + str(getEuro()))
+temp = getTemperatura()
+print("Temp. Máxima: " + str(temp[1]))
+print("Temp. Mínima: " + str(temp[0]))
---
+++ aula05/aula05-metodos.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a + b
+
+v1 = int(input("Digite a: "))
+v2 = int(input("Digite b: "))
+
+resultado = soma(v1, v2)
+
+print("O resultado é " + str(resultado))
+
+print("O resultado é " + str(soma(v1, v2)   ))
